0f87d6a1ee22497de555c6e07abfc604624f2d64
AllanCh <edochacon96@gmail.com>
2019-06-16 16:23:18 -0600

    Route service

--- src/app/services/route.service.ts
+++ src/app/services/route.service.ts
@@ -18,9 +18,9 @@ export class RouteService {
     return this.http.post<RouteInterface>(
       urlApi,
       {
-       idCliente,
-       idMainSite,
-       possibleSites
+       idCliente: idCliente,
+       idMainSite: idMainSite,
+       possibleSites: possibleSites
       },
       {headers: this.headers})
       .pipe(map(data => data));
